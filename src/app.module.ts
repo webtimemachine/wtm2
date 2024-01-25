f07ea1b6ea61b0232593b8c0b033472134118cfa
Federico Suarez <fsuarez@intermediait.com>
2024-01-25 17:55:15 -0300

    boilerplate of AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
