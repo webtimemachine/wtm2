f71d6dfd675c4589e4f8ae940810ecc1c1e99c89
Juan Ignacio Martel <jmartel@intermediait.com>
2024-01-30 08:21:24 -0300

    Persist login data in browser storage

---
+++ extensions/Chrome Web/popup-sign-in-script.js
@@ -0,0 +1,37 @@
+import { is_user_signed_in, } from './auth.js'
+
+const button = document.querySelector('button');
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  const email = document.querySelector('#email').value;
+  const pass = document.querySelector('#password').value;
+
+  if (email && pass) {
+    // send message to background script with email and password
+    chrome.runtime.sendMessage({
+      type: 'login',
+      payload: { email, pass }
+    },
+      function (response) {
+        window.location.replace('./popup.html');
+      })
+  } else {
+    document.querySelector('#email').placeholder = "Enter an email.";
+    document.querySelector('#password').placeholder = "Enter a password.";
+    document.querySelector('#email').style.backgroundColor = 'red';
+    document.querySelector('#password').style.backgroundColor = 'red';
+    document.querySelector('#email').classList.add('white_placeholder');
+    document.querySelector('#password').classList.add('white_placeholder');
+  }
+});
+
+//When popup is ready, check if user is already logged in
+document.addEventListener('DOMContentLoaded', function () {
+  is_user_signed_in().then((res) => {
+    if (res.userStatus) {
+      window.location.replace('./popup.html');
+    }
+  })
+});
