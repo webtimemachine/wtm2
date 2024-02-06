999b9e87d03d67e9250a839898ad3b23e6702a44
Federico Martin Suarez <fsuarez@intermediait.com>
2024-02-06 10:19:49 -0300

    feat: add pagination and query filter to getNavigatioNEntry

---
+++ src/navigation/dtos/get-navigation-entry.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsOptional, IsString } from 'class-validator';
+import { GetPaginationsParamsDto } from 'src/common/dtos';
+
+export class GetNavigationEntryDto extends GetPaginationsParamsDto {
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  query?: string;
+}
