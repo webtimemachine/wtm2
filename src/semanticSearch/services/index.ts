3b5ba252afccbae8a27fe747e59f7f81ef71d2f2
Christian Herrera <christian.herrera@azumo.co>
2024-03-15 12:54:31 -0600

    feat(semantic): add multitenancy support

--- src/semanticSearch/services/index.ts
+++ src/semanticSearch/services/index.ts
@@ -6,33 +6,59 @@ import { OpenAIEmbeddings } from '@langchain/openai';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { appEnv } from 'src/config';
 
-const weaviateArgs: WeaviateLibArgs = {
-  client: weaviate.client({
-    scheme: appEnv.WEAVIATE_SCHEME,
-    host: appEnv.WEAVIATE_HOST,
-  }),
-  indexName: 'WtmIndex',
-  metadataKeys: ['source'],
-  textKey: 'text',
-};
-
 const textSplitter = new RecursiveCharacterTextSplitter({
   chunkSize: 1000,
   chunkOverlap: 200,
   separators: ['\n\n', '\n', '.'],
 });
 
+const client = weaviate.client({
+  scheme: appEnv.WEAVIATE_SCHEME,
+  host: appEnv.WEAVIATE_HOST,
+});
+
 @Injectable()
 export class SemanticProcessor {
   private readonly logger = new Logger(SemanticProcessor.name);
+  private readonly multitenantCollection = 'MultiTenancyCollection';
 
   constructor(private readonly prismaService: PrismaService) {}
 
-  async index(content: string, url: string) {
+  private async vectorStoreArgs(userId: bigint): Promise<WeaviateLibArgs> {
+    try {
+      await client.schema
+        .classCreator()
+        .withClass({
+          class: this.multitenantCollection,
+          multiTenancyConfig: { enabled: true },
+        })
+        .do();
+      this.logger.log('Creating main multitenancy collection');
+    } catch (e) {
+      // the main multitenancy collection only can be created once, instead an error is thrown
+      if (!e.message.includes('already exists')) throw e;
+    }
+
+    await client.schema
+      .tenantsCreator(this.multitenantCollection, [
+        { name: `Tenant-${userId}` },
+      ])
+      .do();
+
+    return {
+      client: client,
+      indexName: this.multitenantCollection,
+      metadataKeys: ['source'],
+      textKey: 'text',
+      tenant: `Tenant-${userId}`,
+    };
+  }
+
+  async index(content: string, url: string, userId: bigint) {
     const exist =
-      (await this.prismaService.navigationEntry.findFirst({
-        where: { url: url },
-      })) !== null;
+      (await this.prismaService.navigationEntry.count({
+        where: { url: url, userId: userId },
+      })) > 0;
     if (!exist) {
       this.logger.log(`Indexing chunks of '${url}'`);
       const documents = await textSplitter.createDocuments(
@@ -43,16 +69,16 @@ export class SemanticProcessor {
       await WeaviateStore.fromDocuments(
         documentChunks,
         new OpenAIEmbeddings({ openAIApiKey: appEnv.OPENAI_ACCESS_TOKEN }),
-        weaviateArgs,
+        await this.vectorStoreArgs(userId),
       );
       this.logger.log(`Chunks of ${url} successfully indexed`);
     } else this.logger.log(`'${url}' was already indexed. Ignoring...`);
   }
 
-  async search(query: string): Promise<Set<string>> {
+  async search(query: string, userId: bigint): Promise<Set<string>> {
     const store = await WeaviateStore.fromExistingIndex(
       new OpenAIEmbeddings({ openAIApiKey: appEnv.OPENAI_ACCESS_TOKEN }),
-      weaviateArgs,
+      await this.vectorStoreArgs(userId),
     );
     const retriever = store.asRetriever({ k: 5 });
     const relevantChunks = await retriever.getRelevantDocuments(query);
