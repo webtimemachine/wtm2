e9dd34a2d2da3e7e4e945791500b7f8d06ede10c
Bautista Carpintero <bcarpintero@intermediait.com>
2024-03-21 16:19:34 -0300

    async await optimizations

--- extensions/Chrome Web/settings.js
+++ extensions/Chrome Web/settings.js
@@ -1,20 +1,21 @@
-import { API_URL } from './consts.js'
+import { API_URL } from './consts.js';
 
 // -- Close Config / Setting button -- //
 const closeSettingButton = document.getElementById('close-settings-button');
 
 closeSettingButton.addEventListener('click', function () {
-    window.location.replace('./popup.html');
-})
+  window.location.replace('./popup.html');
+});
 
 document.addEventListener('DOMContentLoaded', function () {
-    chrome.runtime.sendMessage({ type: "getSettingInfo" }, function (response) {
-        if (response.user_info?.user) { //Display user email connected & backend information
-            const userDiv = document.getElementById('user-connected');
-            userDiv.innerHTML = `User: ${response.user_info.user.email}`
+  chrome.runtime.sendMessage({ type: 'getSettingInfo' }, function (response) {
+    if (response.user_info?.user) {
+      //Display user email connected & backend information
+      const userDiv = document.getElementById('user-connected');
+      userDiv.innerHTML = `User: ${response.user_info.user.email}`;
 
-            const apiDiv = document.getElementById('backend-connected');
-            apiDiv.innerHTML = `Backend API: ${API_URL}`
-        }
-    });
+      const apiDiv = document.getElementById('backend-connected');
+      apiDiv.innerHTML = `Backend API: ${API_URL}`;
+    }
+  });
 });
