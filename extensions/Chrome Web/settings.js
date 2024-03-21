4b0ebd1abb513fd14a48fa78069c783b3a28003e
Juan Ignacio Martel <jmartel@intermediait.com>
2024-03-21 09:33:43 -0300

    fix: remove console log

--- extensions/Chrome Web/settings.js
+++ extensions/Chrome Web/settings.js
@@ -9,7 +9,6 @@ closeSettingButton.addEventListener('click', function () {
 
 document.addEventListener('DOMContentLoaded', function () {
     chrome.runtime.sendMessage({ type: "getSettingInfo" }, function (response) {
-        console.log('response', response);
         if (response.user_info?.user) { //Display user email connected & backend information
             const userDiv = document.getElementById('user-connected');
             userDiv.innerHTML = `User: ${response.user_info.user.email}`
