839328c4998f3578dbbb8be5553ce5b8e53f0ae2
Christian Herrera <christian.herrera@azumo.co>
2024-03-22 17:37:50 -0600

    refactor(queryHistory): try to get a Query entry before creating a new one

--- src/query/services/index.ts
+++ src/query/services/index.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../common/services';
+import { Query } from '@prisma/client';
 
 @Injectable()
 export class QueryService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async newEntry(
-    query: string,
-    isSemantic: boolean,
-    navigationEntriesIds: bigint[],
-  ) {
-    const newQueryEntry = await this.prismaService.query.create({
-      data: { query: query, semantic: isSemantic },
+  async newEntry(query: string, navigationEntriesIds: bigint[]) {
+    let queryEntry: Query | null = await this.prismaService.query.findUnique({
+      where: { query: query },
     });
+    if (queryEntry == null) {
+      queryEntry = await this.prismaService.query.create({
+        data: { query: query },
+      });
+    }
     const newQueryEntries = navigationEntriesIds.map((entryId) => ({
-      queryId: newQueryEntry.id,
+      queryId: queryEntry!.id,
       navigationEntryId: entryId,
     }));
     await this.prismaService.navigationEntryQuery.createMany({
