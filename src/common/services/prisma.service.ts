50f64a433b560347d5fbdf02f164f4b49d8486a9
Francisco Cabral <fcabral@intermediait.com>
2024-03-25 16:49:02 -0300

    feature: user delete added

--- src/common/services/prisma.service.ts
+++ src/common/services/prisma.service.ts
@@ -1,9 +1,40 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  INestApplication,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private logger = new Logger('PrismaService');
+
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+      ],
+    });
+  }
+
   async onModuleInit(): Promise<void> {
     await this.$connect();
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.$on('query', async (e) => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      this.logger.log(`${e.query} ${e.params}`);
+    });
+  }
+
+  async enableShutdownHooks(app: INestApplication): Promise<void> {
+    process.on('beforeExit', async () => {
+      await app.close();
+    });
   }
 }
