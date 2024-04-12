d26edcf110d1b2639b5c38a16451e8c664b6664e
Bautista Carpintero <bcarpintero@intermediait.com>
2024-04-12 16:44:58 -0300

    ts on manifest file

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,16 +1,12 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { crx } from '@crxjs/vite-plugin';
 
-import {crx} from '@crxjs/vite-plugin';
-import {ManifestV3Export} from '@crxjs/vite-plugin';
-
-import manifestJSON from './manifest.json';
-
-const manifest: ManifestV3Export = manifestJSON as ManifestV3Export;
+import { manifest } from './manifest';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({manifest})],
+  plugins: [react(), crx({ manifest })],
   server: {
     port: 3000,
   },
