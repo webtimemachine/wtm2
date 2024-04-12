fb75cfb3f93a2a7fc0bf44fbaadc880b4eac3706
Bautista Carpintero <bcarpintero@intermediait.com>
2024-04-12 12:34:15 -0300

    update vite settings

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+      },
+    },
+  },
+});
