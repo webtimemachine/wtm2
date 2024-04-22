0c4a39f9c8b599b4d16e3a477e5c1d310659b8cb
Juan Ignacio Martel <jmartel@intermediait.com>
2024-04-22 18:12:02 -0300

    SmallCloseIcon added

--- app/src/screens/navigation-entries.screen.tsx
+++ app/src/screens/navigation-entries.screen.tsx
@@ -3,7 +3,7 @@ import { Button, Checkbox, Input, Text, IconButton } from '@chakra-ui/react'
 import { CompleteNavigationEntryDto } from '../background/interfaces/navigation-entry'
 import { useNavigationEntries } from '../hooks/use-navigation-entries.hook'
 import { useLogout } from '../hooks/use-logout.hook'
-import { SettingsIcon } from '@chakra-ui/icons'
+import { SettingsIcon, SmallCloseIcon } from '@chakra-ui/icons'
 
 export const NavigationEntriesScreen: React.FC<object> = () => {
   const { data } = useNavigationEntries()
@@ -32,8 +32,9 @@ export const NavigationEntriesScreen: React.FC<object> = () => {
             </div>
             <div className='flex flex-col w-full'>
               {data && data.items.length ? data.items.map((element: CompleteNavigationEntryDto) => {
-                return <div key={element.id} className='flex bg-white px-2 py-1 rounded mb-1 gap-1'>
+                return <div key={element.id} className='flex w-full bg-white px-2 py-1 rounded mb-1 items-center justify-between'>
                   <Text fontSize={'small'} className='overflow-hidden truncate'>{new Date(element.navigationDate).toLocaleString()} - {element.title}</Text>
+                  <SmallCloseIcon boxSize={5} />
                 </div>
               }) : <div>
                 <Text fontSize={'small'}>No results found. Try different search terms!</Text>
