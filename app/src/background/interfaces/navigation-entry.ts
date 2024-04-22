18d2475c08b180e420386d464be8727604d308da
Juan Ignacio Martel <jmartel@intermediait.com>
2024-04-22 18:23:27 -0300

    Remove duplicated code

--- app/src/background/interfaces/navigation-entry.ts
+++ app/src/background/interfaces/navigation-entry.ts
@@ -1,48 +1,4 @@
-interface UABrowser {
-  name?: string;
-  version?: string;
-  major?: string;
-}
-interface UADevice {
-  model?: string;
-  type?: string;
-  vendor?: string;
-}
-interface UAEngine {
-  name?: string;
-  version?: string;
-}
-interface UAOS {
-  name?: string;
-  version?: string;
-}
-interface UACPU {
-  architecture?: string;
-}
-interface UAResult {
-  ua: string;
-  browser: UABrowser;
-  device: UADevice;
-  engine: UAEngine;
-  os: UAOS;
-  cpu: UACPU;
-}
-
-interface DeviceDto {
-  id: number;
-  deviceKey: string;
-  userAgent?: string;
-  uaResult?: UAResult;
-}
-
-interface UserDeviceDto {
-  id: number;
-  userId: number;
-  deviceId: number;
-  isCurrentDevice: boolean;
-  deviceAlias: string;
-  device: DeviceDto;
-}
+import { UserDeviceResponse } from './user-device.interface';
 
 export interface CompleteNavigationEntryDto {
   id: number;
@@ -51,7 +7,7 @@ export interface CompleteNavigationEntryDto {
   navigationDate: Date;
   userId: number;
   userDeviceId: number;
-  userDevice: UserDeviceDto;
+  userDevice: UserDeviceResponse;
   expirationDate?: Date;
 }
 
