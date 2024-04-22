23e8a3aaa6bb7f3555add1d6115d800e746b4c34
Juan Ignacio Martel <jmartel@intermediait.com>
2024-04-22 15:06:30 -0300

    handleGetNavigationEntries added

--- app/src/background/interfaces/index.ts
+++ app/src/background/interfaces/index.ts
@@ -1,22 +1,25 @@
-import { SayHelloData, SayHelloResponse } from './say-hello.interface';
-import { GetVersionResponse } from './get-version.interface';
-import { LoginData, LoginResponse } from './login.interface';
+import { SayHelloData, SayHelloResponse } from './say-hello.interface'
+import { GetVersionResponse } from './get-version.interface'
+import { LoginData, LoginResponse } from './login.interface'
+import { GetNavigationEntriesResponse } from './navigation-entry'
 
-export type BackgroundMessageType = 'say-hello' | 'get-version' | 'login';
+export type BackgroundMessageType = 'say-hello' | 'get-version' | 'login' | 'get-navigation-entries'
 
 export type BackgroundMessageDataMap = {
-  'say-hello': SayHelloData;
-  'get-version': undefined;
-  login: LoginData;
-};
+  'say-hello': SayHelloData
+  'get-version': undefined
+  login: LoginData
+  'get-navigation-entries': undefined
+}
 export type BackgroundMessageResponseMap = {
-  'say-hello': SayHelloResponse;
-  'get-version': GetVersionResponse;
-  login: LoginResponse;
-};
+  'say-hello': SayHelloResponse
+  'get-version': GetVersionResponse
+  login: LoginResponse
+  'get-navigation-entries': GetNavigationEntriesResponse
+}
 
 export interface BackgroundMessagePayload<T extends BackgroundMessageType> {
-  data: BackgroundMessageDataMap[T];
+  data: BackgroundMessageDataMap[T]
 }
 
 export type BackgroundMessageHandler<T extends BackgroundMessageType> = (
@@ -24,4 +27,4 @@ export type BackgroundMessageHandler<T extends BackgroundMessageType> = (
     response: BackgroundMessageResponseMap[T] | { error: string },
   ) => void,
   payload: BackgroundMessagePayload<T>,
-) => Promise<void>;
+) => Promise<void>
