ddd42d2b790b0ab1e6d454dcee11abcd9bdfc96c
Juan Ignacio Martel <jmartel@intermediait.com>
2024-04-22 17:35:39 -0300

    checkbox and setting icon added

--- app/src/screens/navigation-entries.screen.tsx
+++ app/src/screens/navigation-entries.screen.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Button, Input, Text } from '@chakra-ui/react'
+import { Button, Checkbox, Input, Text, IconButton } from '@chakra-ui/react'
 import { CompleteNavigationEntryDto } from '../background/interfaces/navigation-entry'
 import { useNavigationEntries } from '../hooks/use-navigation-entries.hook'
 import { useLogout } from '../hooks/use-logout.hook'
+import { SettingsIcon } from '@chakra-ui/icons'
 
 export const NavigationEntriesScreen: React.FC<object> = () => {
   const { data } = useNavigationEntries()
@@ -12,14 +13,22 @@ export const NavigationEntriesScreen: React.FC<object> = () => {
 
   return (
     <>
-      <div className="flex flex-col p-5 bg-slate-100 min-h-screen items-center w-full">
+      <div className="flex flex-col px-5 py-3 bg-slate-100 min-h-screen items-center w-full">
+        <div className='flex w-full justify-end'>
+          <IconButton aria-label='Settings icon' onClick={() => console.log('hola')}>
+            <SettingsIcon boxSize={5} />
+          </IconButton>
+        </div>
         <div className='pb-4'>
           <Text fontSize={'xx-large'} fontWeight={'bold'}>WebTM</Text>
         </div>
         <div className='flex flex-col w-full min-h-[400px] justify-between'>
           <div>
-            <div className='p-4 flex w-full'>
+            <div className='pt-4 pb-2 flex w-full'>
               <Input type='text' name='search' placeholder='Search' backgroundColor={'white'} />
+            </div>
+            <div className='flex pb-2'>
+              <Checkbox>Is semantic?</Checkbox>
             </div>
             <div className='flex flex-col w-full'>
               {data && data.items.length ? data.items.map((element: CompleteNavigationEntryDto) => {
