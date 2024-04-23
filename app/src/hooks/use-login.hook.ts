f806e5c3564ebc4ff9c889ea1ca4714348d906d9
Bautista Carpintero <bcarpintero@intermediait.com>
2024-04-23 14:30:22 -0300

    add validations to login screen

--- app/src/hooks/use-login.hook.ts
+++ app/src/hooks/use-login.hook.ts
@@ -3,14 +3,10 @@ import { useMutation } from '@tanstack/react-query';
 
 import { useSendBackgroundMessage } from './use-send-message.hook';
 import { LoginData } from '../background/interfaces/login.interface';
-import { useNavigationStore } from '../store';
-import { useIsLoggedIn } from './use-logged-in.hook';
 
 export const useLogin = () => {
   const toast = useToast();
   const { sendBackgroundMessage } = useSendBackgroundMessage();
-  const { navigateTo } = useNavigationStore();
-  const { isLoggedIn } = useIsLoggedIn();
 
   const login = (data: LoginData) => sendBackgroundMessage('login', data);
 
@@ -25,7 +21,6 @@ export const useLogin = () => {
         duration: 3000,
         isClosable: true,
       });
-      navigateTo('navigation-entries');
     },
     onError: (error) => {
       console.error(error);
@@ -39,14 +34,5 @@ export const useLogin = () => {
     retry: false,
   });
 
-  const navigateToMainPageIfIsLogged = async () => {
-    //TODO Verify if token are expired before navigation
-    const isUserLogged = await isLoggedIn();
-
-    if (isUserLogged) {
-      navigateTo('navigation-entries');
-    }
-  };
-
-  return { loginMutation, navigateToMainPageIfIsLogged };
+  return { loginMutation };
 };
