12872f0d25cdd1a180c3d5b4a30be78003f9542f
Juan Ignacio Martel <jmartel@intermediait.com>
2024-04-24 12:01:56 -0300

    logout function move it to settings page

--- app/src/screens/settings.screen.tsx
+++ app/src/screens/settings.screen.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Text, IconButton } from '@chakra-ui/react';
 import { CloseIcon } from '@chakra-ui/icons';
+import { useNavigationStore } from '../store';
+import { useLogout } from '../hooks/use-logout.hook';
 
 export const SettingsScreen: React.FC<object> = () => {
+  const { navigateBack } = useNavigationStore();
+  const { logout } = useLogout();
+
   return (
     <>
       <div className='flex flex-col px-5 py-3 bg-slate-100 min-h-screen items-center w-full'>
         <div className='flex w-full justify-end'>
           <IconButton aria-label='Settings icon'>
-            <CloseIcon boxSize={5} />
+            <CloseIcon boxSize={5} onClick={() => navigateBack()} />
           </IconButton>
         </div>
         <div className='pb-4'>
@@ -16,7 +21,17 @@ export const SettingsScreen: React.FC<object> = () => {
             Settings
           </Text>
         </div>
-        <div className='flex flex-col w-full min-h-[400px] justify-between'></div>
+        <div className='flex flex-col w-full min-h-[400px] justify-between'>
+          <div className='flex w-full py-2 cursor-pointer border-b border-solid border-[#333]'>
+            <Text fontSize={'medium'}>Preferencies</Text>
+          </div>
+        </div>
+        <div
+          className='flex w-full py-2 cursor-pointer justify-center'
+          onClick={logout}
+        >
+          <Text fontSize={'medium'}>Logout</Text>
+        </div>
       </div>
     </>
   );
