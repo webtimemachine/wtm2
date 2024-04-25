3f78f98f6ab268538b8df81e09f376aef46dfeea
Juan Ignacio Martel <jmartel@intermediait.com>
2024-04-25 09:39:14 -0300

    handleGetActiveSessions function created and interface files renamed

--- app/src/hooks/use-navigation-entries.hook.ts
+++ app/src/hooks/use-navigation-entries.hook.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { useSendBackgroundMessage } from './use-send-message.hook';
-import { GetNavigationEntriesData } from '../background/interfaces/navigation-entry';
+import { GetNavigationEntriesData } from '../background/interfaces/navigation-entry.interface';
 
 export const useNavigationEntries = (params: GetNavigationEntriesData) => {
   const { sendBackgroundMessage } = useSendBackgroundMessage();
