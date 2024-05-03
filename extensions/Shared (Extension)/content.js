2d74122647e8958221eb4266735b54641e4a9bd4
Bautista Carpintero <bcarpintero@intermediait.com>
2024-05-03 14:42:31 -0300

    remove old extension folder

--- extensions/Shared (Extension)/content.js
+++
@@ -1,7 +0,0 @@
-
-// This code runs in the context of the webpage
-let currentURL = window.location.href;
-
-// Send a message to the global page with the current URL
-
-safari.extension.dispatchMessage("SaveURL", { url: currentURL });
