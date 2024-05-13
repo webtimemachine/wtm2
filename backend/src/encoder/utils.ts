6dc1eb9239769dee7a295493e423d5ca6516b603
Christian Herrera <christian.herrera@azumo.co>
2024-05-13 17:18:28 -0600

    feat: image captioning utility with OpenAI integration

---
+++ backend/src/encoder/utils.ts
@@ -0,0 +1,43 @@
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  ImagePromptTemplate,
+  PromptTemplate,
+  SystemMessagePromptTemplate,
+} from '@langchain/core/prompts';
+import { ChatOpenAI } from '@langchain/openai';
+import { LLMChain } from 'langchain/chains';
+import { appEnv } from '../config';
+
+const captionerModel = new ChatOpenAI({
+  modelName: 'gpt-4-vision-preview',
+  maxTokens: 256,
+  temperature: 0,
+  openAIApiKey: appEnv.OPENAI_ACCESS_TOKEN,
+});
+
+const humanPrompt = new HumanMessagePromptTemplate([
+  PromptTemplate.fromTemplate(
+    'Can you please tell me what is displayed on this image? Ignore texts in it',
+  ),
+  new ImagePromptTemplate({
+    template: { url: '{image}' },
+    inputVariables: ['image'],
+  }),
+]);
+
+const mainPrompt = ChatPromptTemplate.fromMessages([
+  SystemMessagePromptTemplate.fromTemplate(
+    'You are an image captioner, returning a 20 words summary about what you see in any image. REMEMBER to keep it short',
+  ),
+  humanPrompt,
+]);
+
+const chain = new LLMChain({ llm: captionerModel, prompt: mainPrompt });
+
+export const caption = async (image: string): Promise<string> => {
+  const response = await chain.invoke({
+    image: `data:image/jpeg;base64,${image}`,
+  });
+  return response['text'];
+};
