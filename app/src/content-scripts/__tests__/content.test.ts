41255881f9d7bf6b3d93445494b903c0d30bc7f6
Juan Ignacio Martel <jmartel@intermediait.com>
2024-05-15 09:36:47 -0300

    Content script test added

---
+++ app/src/content-scripts/__tests__/content.test.ts
@@ -0,0 +1,84 @@
+import { apiClient } from '../../utils/api.client';
+import { postNavigationEntry } from '../content';
+
+// Mocking the DOMtoString function
+jest.mock('../../utils', () => ({
+  DOMtoString: jest.fn().mockReturnValue('<div>Mock HTML Content</div>'),
+}));
+
+jest.mock('../../utils/api.client', () => ({
+  apiClient: {
+    securedFetch: jest.fn(),
+  },
+}));
+
+// Mocking the chrome storage local API
+global.chrome = {
+  storage: {
+    local: {
+      get: jest.fn().mockImplementationOnce(() => {
+        return { accessToken: 'mockAccessToken' };
+      }),
+    },
+  },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+} as any;
+
+describe('postNavigationEntry', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should post navigation entry successfully when accessToken is available and URL is valid', async () => {
+    const mockUrl = 'https://example.com';
+    const mockTitle = 'Example Title';
+
+    (apiClient.securedFetch as jest.Mock).mockResolvedValueOnce({
+      status: 200,
+      json: jest.fn(),
+    });
+
+    global.window = Object.create(window);
+    Object.defineProperty(window, 'location', {
+      value: { href: mockUrl },
+    });
+    Object.defineProperty(document, 'title', {
+      value: mockTitle,
+    });
+
+    await postNavigationEntry();
+
+    expect(global.chrome.storage.local.get).toHaveBeenCalledWith([
+      'accessToken',
+    ]);
+    expect(apiClient.securedFetch).toHaveBeenCalled();
+  });
+
+  it('should not post navigation entry when accessToken is not available', async () => {
+    global.chrome = {
+      storage: {
+        local: {
+          get: jest.fn().mockImplementationOnce(() => {
+            return undefined;
+          }),
+        },
+      },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any;
+
+    await postNavigationEntry();
+
+    expect(apiClient.securedFetch).not.toHaveBeenCalled();
+  });
+
+  it('should not post navigation entry when URL is invalid', async () => {
+    global.window = Object.create(window);
+    Object.defineProperty(window, 'location', {
+      value: { href: 'chrome://invalid' },
+    });
+
+    await postNavigationEntry();
+
+    expect(apiClient.securedFetch).not.toHaveBeenCalled();
+  });
+});
