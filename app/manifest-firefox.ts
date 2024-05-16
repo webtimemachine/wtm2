04ac4be8bac2121b60e6f2848924b67094a1568c
Bautista Carpintero <bcarpintero@intermediait.com>
2024-05-16 17:38:41 -0300

    manifest updates

--- app/manifest-firefox.ts
+++ app/manifest-firefox.ts
@@ -3,9 +3,9 @@ import { ManifestV3Export } from '@crxjs/vite-plugin';
 export const manifestFirefox: ManifestV3Export = {
   manifest_version: 3,
   name: 'WebTM',
-  version: '1.5.1',
+  version: '1.5.2',
   description:
-    'WebTM is a cross-platform solution to integrate the navigation history between desktop and mobile web browsers. Focusing on providing a solution for integration between Google Chrome, Firefox and Safari iOS.',
+    'WebTM is a cross-platform solution to integrate the navigation history between desktop and mobile web browsers.',
   browser_specific_settings: {
     gecko: {
       id: '{5790cffd-a2b7-4cb6-ad05-c5b955ddee3e}',
