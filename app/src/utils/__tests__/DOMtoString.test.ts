eca44873196b299fadabf5e1ab26b1e4183e8bea
Christian Herrera <christian.herrera@azumo.co>
2024-05-16 16:56:52 -0600

    test: image sources extraction

--- app/src/utils/__tests__/DOMtoString.test.ts
+++ app/src/utils/__tests__/DOMtoString.test.ts
@@ -1,4 +1,4 @@
-import { DOMtoString } from '../DOMtoString';
+import { DOMtoString, getImages } from '../DOMtoString';
 
 describe('DOMtoString', () => {
   beforeEach(() => {
@@ -33,4 +33,24 @@ describe('DOMtoString', () => {
     const expected = 'ERROR: querySelector failed to find node';
     expect(result).toEqual(expected);
   });
+});
+
+describe('getImages', () => {
+  beforeEach(() => {
+    document.body.innerHTML = ''; // Clear the document body before each test
+  });
+
+  it('should get valid images', () => {
+    const expected = ['https://media.image.com/test/1', 'https://media.image.com/test/2']
+    document.body.innerHTML = `
+    <div>
+      <img src="https://media.image.com/test/1" height="1080" width="1920" loading="lazy">
+      <img src="https://media.image.com/test/2" height="1000" width="1920" loading="lazy">
+      <img src="https://media.image.com/test/3" height="250" width="300" loading="lazy">
+    </div>`;
+
+    const result = getImages()
+    expect(result).toEqual(expected);
+  });
+
 });
