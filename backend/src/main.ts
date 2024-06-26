6b2bcb8d2c70c1ac82f9c8d58a2d5ccdafeb14a6
Bautista Carpintero <bcarpintero@intermediait.com>
2024-06-26 09:57:38 -0300

    sentry integration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,39 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { SecuritySchemeObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
 
-import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 import { appEnv } from './config';
 import { getVersion } from './getVersion';
 import { options } from './swagger-options';
+
+import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+import { SentryFilter } from './sentry.filter';
+
+const initSentry = () => {
+  Sentry.init({
+    dsn: appEnv.SENTRY_DSN,
+    integrations: [nodeProfilingIntegration()],
+    tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0,
+  });
+};
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
 
   const app = await NestFactory.create(AppModule);
+
+  if (appEnv.SENTRY_DSN) {
+    initSentry();
+    const { httpAdapter } = app.get(HttpAdapterHost);
+    app.useGlobalFilters(new SentryFilter(httpAdapter));
+  }
+
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.use(bodyParser.json({ limit: '50mb' }));
 
