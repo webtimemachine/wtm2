02f117ca05b0df69f1e37d8ae765210597e297bb
Bautista Carpintero <bcarpintero@intermediait.com>
2024-08-09 17:00:20 -0300

    lib integration on webpage wip

--- webpage/src/utils/api.client.ts
+++ webpage/src/utils/api.client.ts
@@ -1,136 +1,39 @@
 import { authStore } from '../store';
-import {
-  LoginData,
-  LoginResponse,
-  VerifyEmailResponse,
-  isLoginRes,
-} from '../interfaces/login.interface';
+import { ApiClient } from 'wtm-lib/ApiClient';
 
-class ApiClient {
-  async fetch(endpoint: string, init: RequestInit = {}): Promise<Response> {
+export const apiClient: ApiClient = new ApiClient({
+  getServerUrl: async () => {
     const { serverUrl } = authStore.getState();
-    init = {
-      ...init,
-      headers: {
-        'Content-Type': 'application/json',
-        ...init?.headers,
-      },
-    };
+    return (serverUrl as string) || '';
+  },
 
-    const res = await fetch(new URL(endpoint, serverUrl), init);
-    return res;
-  }
+  getAccessToken: async () => {
+    const { accessToken } = authStore.getState();
+    return (accessToken as string) || '';
+  },
+  getRefreshToken: async () => {
+    const { refreshToken } = authStore.getState();
+    return (refreshToken as string) || '';
+  },
+  getPartialToken: async () => {
+    const { partialToken } = authStore.getState();
+    return (partialToken as string) || '';
+  },
+  getRecoveryToken: async () => {
+    const { recoveryToken } = authStore.getState();
+    return (recoveryToken as string) || '';
+  },
 
-  async securedFetch(
-    endpoint: string,
-    init: RequestInit = {},
-  ): Promise<Response> {
-    const { serverUrl, accessToken } = authStore.getState();
-
-    init = {
-      ...init,
-      headers: {
-        'Content-Type': 'application/json',
-        ...init?.headers,
-      },
-    };
-
-    try {
-      if (accessToken) {
-        init = {
-          ...init,
-          headers: {
-            ...init?.headers,
-            Authorization: `Bearer ${accessToken}`,
-          },
-        };
-      }
-      const res = await fetch(new URL(endpoint, serverUrl), init);
-
-      if (res.status === 401) {
-        const jsonRes = await res.json();
-        console.error('ApiClient', { endpoint, response: jsonRes });
-        throw new Error('Unauthorized');
-      }
-
-      return res;
-    } catch (error: any) {
-      if (error?.message === 'Unauthorized') {
-        await this.refresh();
-        console.log('ApiClient Retrying request after refresh', { endpoint });
-        return this.securedFetch(endpoint, init);
-      } else {
-        throw error;
-      }
-    }
-  }
-
-  async login(data: LoginData): Promise<LoginResponse | VerifyEmailResponse> {
-    const { serverUrl } = authStore.getState();
-
-    try {
-      const res = await fetch(new URL('/api/auth/login', serverUrl), {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (res.status !== 200) {
-        const errorJson = await res.json();
-        throw new Error(errorJson?.message || 'Login Error');
-      }
-
-      const loginResponse: LoginResponse | VerifyEmailResponse =
-        await res.json();
-      if (isLoginRes(loginResponse)) {
-        const { accessToken, refreshToken } = loginResponse;
-
-        authStore.setState({
-          accessToken,
-          refreshToken,
-        });
-      } else {
-        const { partialToken } = loginResponse;
-        authStore.setState({
-          partialToken,
-        });
-      }
-
-      return loginResponse;
-    } catch (error) {
-      console.error('ApiClient login', error);
-      throw error;
-    }
-  }
-
-  async refresh(): Promise<void> {
-    const { serverUrl, refreshToken } = authStore.getState();
-
-    try {
-      const res = await fetch(new URL('/api/auth/refresh', serverUrl), {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${refreshToken}`,
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (res.status !== 200) {
-        throw new Error('Unauthorized');
-      }
-
-      const data = await res.json();
-      authStore.setState({
-        accessToken: data.accessToken,
-        refreshToken: data.refreshToken,
-      });
-    } catch (error) {
-      console.error('ApiClient Refresh Error', error);
-      throw error;
-    }
-  }
-}
-
-export const apiClient: ApiClient = new ApiClient();
+  setAccessToken: async (accessToken: string) => {
+    authStore.setState({ accessToken });
+  },
+  setRefreshToken: async (refreshToken: string) => {
+    authStore.setState({ refreshToken });
+  },
+  setPartialToken: async (partialToken: string) => {
+    authStore.setState({ partialToken });
+  },
+  setRecoveryToken: async (recoveryToken: string) => {
+    authStore.setState({ recoveryToken });
+  },
+});
