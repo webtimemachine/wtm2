cf1bb6a92843b7d4ca5aea27dead0d654bf3e778
aferrentinointermedia <aferrentino@intermediait.com>
2024-08-14 08:50:18 -0300

    Fixed the way webpage content it's displayed and gathered from web

--- webpage/src/app/navigation-entries/page.tsx
+++ webpage/src/app/navigation-entries/page.tsx
@@ -15,40 +15,59 @@ import {
 import React, { useEffect, useState } from 'react';
 
 import {
-  ArrowLeftIcon,
-  ArrowRightIcon,
   ChevronDownIcon,
   ChevronLeftIcon,
   ChevronRightIcon,
   ChevronUpIcon,
   HamburgerIcon,
-  SettingsIcon,
   SmallCloseIcon,
 } from '@chakra-ui/icons';
 import { BsStars } from 'react-icons/bs';
 import {
   useDeleteNavigationEntry,
-  useLogout,
   useNavigationEntries,
 } from '../../hooks';
 import {
   CompleteNavigationEntryDto,
   NavEntryProps,
 } from '../../interfaces/navigation-entry.interface';
-
+import './markdown-styles.css'
 import { getBrowserIconFromDevice } from '../../utils';
 import clsx from 'clsx';
 
 import { CustomDrawer } from '@/components/custom-drawer';
+import Markdown from 'react-markdown';
 const truncateString = (str: string, maxLength: number) => {
   return str.length <= maxLength ? str : str.slice(0, maxLength) + '...';
 };
-
+const getPreProcessedMarkDown = (relevantSegment: string) => {
+  const emptyListPatterns = [
+    /\*\n(\*\n)*/g, // matches lines with only *
+    /-\n(-\n)*/g, // matches lines with only -
+    /- \*\*\n(\*+\n)*/g, // matches lines with only - ** and *+
+    /\n\s*\*\*\n(\s*\*+\n)*/g, // matches lines with ** and *+ with spaces
+    /- \*\n(\s*\*\n)*/g, // matches lines with - * and *+
+    /\n\s*\*\n(\s*\*\n)*/g, // matches lines with * and *+ with spaces
+    /\n\s*-\n(\s*-\n)*/g // matches lines with - and -+ with spaces
+  ];
+  let markdown = relevantSegment;
+  emptyListPatterns.forEach(pattern => {
+    markdown = markdown.replace(pattern, '');
+  });
+  markdown = markdown.replace(/\n{2,}/g, '\n\n');
+  return markdown || "";
+}
 const RelevantSegment = ({ relevantSegment }: { relevantSegment: string }) => {
+
+  const markdown = getPreProcessedMarkDown(relevantSegment);
+
   return (
     <div>
       <p className='font-semibold mb-4'>Most relevant match</p>
-      <p className='text-xs'>{relevantSegment}</p>
+      <p className='text-xs'></p>
+      <div className='markdown-content'>
+        <Markdown>{markdown}</Markdown>
+      </div>
     </div>
   );
 };
