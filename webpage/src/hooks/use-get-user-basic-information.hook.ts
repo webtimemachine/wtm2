3cea711373273573300d488a4a50ae1e9e5840dc
aferrentinointermedia <aferrentino@intermediait.com>
2024-08-23 18:56:02 -0300

    Improved user data pick

---
+++ webpage/src/hooks/use-get-user-basic-information.hook.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { apiClient } from '../utils/api.client';
+import { useHandleSessionExpired } from '.';
+
+export const useGetBasicUserIngormation = () => {
+  const { handleSessionExpired } = useHandleSessionExpired();
+
+  apiClient.setHandleSessionExpired(handleSessionExpired);
+
+  const basicUserInformationQuery = useQuery({
+    queryKey: ['getBasicUserInformationQuery'],
+    queryFn: () => apiClient.getBasicUserInformation(),
+    enabled: true,
+  });
+
+  return { basicUserInformationQuery };
+};
