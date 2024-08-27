32410505439798bfd377ad389d02944f9069eb94
Diego Coscolla <dcoscolla@intermediait.com>
2024-08-27 11:56:46 -0300

    Add auto-generate secure password feature to extension

---
+++ app/src/utils/generateSecurePassword.ts
@@ -0,0 +1,13 @@
+export function generateSecurePassword(length = 12) {
+  const charset =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?,./';
+  let password = '';
+  const array = new Uint32Array(length);
+  window.crypto.getRandomValues(array);
+
+  for (let i = 0; i < length; i++) {
+    password += charset[array[i] % charset.length];
+  }
+
+  return password;
+}
