895410ee11f3fc071d5398345838f1886f1ed621
Diego Coscolla <dcoscolla@intermediait.com>
2024-09-05 17:26:30 -0300

    Fix tests

--- backend/src/user/services/user.service.spec.ts
+++ backend/src/user/services/user.service.spec.ts
@@ -36,6 +36,7 @@ const existingUser = {
     navigationEntryExpirationInDays: 120,
     enableImageEncoding: true,
     enableExplicitContentFilter: true,
+    enableStopTracking: true,
     createdAt: new Date(),
     updateAt: new Date(),
   },
