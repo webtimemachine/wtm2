d200fe3808a7a10d110b729d6c98b6806d87d669
Diego Coscolla <dcoscolla@intermediait.com>
2024-09-26 11:14:51 -0300

    Fix tests errors and update pkg lock

--- packages/utils/jest.config.js
+++ packages/utils/jest.config.js
@@ -2,7 +2,6 @@ const baseConfig = require('../../jest.config.base.js');
 
 module.exports = {
   ...baseConfig,
-  name: '@wtm/utils',
   displayName: 'Utils',
   testEnvironment: 'jsdom',
   rootDir: '.',
