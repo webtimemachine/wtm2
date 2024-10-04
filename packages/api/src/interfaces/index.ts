33f7816b9e8cf98c1bc02ae1cbd0c32c60fbd8d9
aferrentinointermedia <aferrentino@intermediait.com>
2024-10-04 16:41:48 -0300

    bulk delete navigation entries

--- packages/api/src/interfaces/index.ts
+++ packages/api/src/interfaces/index.ts
@@ -1,55 +1,56 @@
-export { type GetVersionResponse } from './get-version.interface';
+export { type GetVersionResponse } from "./get-version.interface";
 export {
   type LoginData,
   type LoginResponse,
   type VerifyEmailResponse,
   isLoginRes,
-} from './login.interface';
+} from "./login.interface";
 export {
   type DeleteNavigationEntriesData,
   type GetNavigationEntriesData,
   type GetNavigationEntriesResponse,
   type CreateNavigationEntry,
   type CompleteNavigationEntryDto,
-} from './navigation-entry.interface';
+  type BulkDeleteNavigationEntriesData,
+} from "./navigation-entry.interface";
 export {
   type UpdatePreferenciesData,
   type PreferenciesResponse,
-} from './preferences.interface';
-export { type CloseActiveSessionsData } from './close-active-session';
-export { type BasicResponse } from './basic-response';
+} from "./preferences.interface";
+export { type CloseActiveSessionsData } from "./close-active-session";
+export { type BasicResponse } from "./basic-response";
 export {
   type UpdateDeviceAliasData,
   type UserDevice,
   type Device,
-} from './user-device.interface';
+} from "./user-device.interface";
 export {
   type SignUpData,
   type SignUpResponse,
   type SignUpErrorResponse,
-} from './sign-up.interface';
-export { type ActiveSession } from './active-sessons.interface';
+} from "./sign-up.interface";
+export { type ActiveSession } from "./active-sessons.interface";
 export {
   type ResendCodeResponse,
   type ResendCodeErrorResponse,
-} from './resend-code.interface';
+} from "./resend-code.interface";
 export {
   type VerifyCodeData,
   type VerifyCodeErrorResponse,
-} from './verify-code.interface';
+} from "./verify-code.interface";
 export {
   type RecoverPasswordData,
   type RecoverPasswordResponse,
-} from './recover-password.interface';
+} from "./recover-password.interface";
 export {
   type ValidateRecoveryCodeData,
   type ValidateRecoveryCodeResponse,
   type ValidateRecoveryCodeErrorResponse,
-} from './validate-recovery-code.interface';
+} from "./validate-recovery-code.interface";
 export {
   type RestorePasswordData,
   type RestorePasswordErrorResponse,
-} from './restore-password.interface';
-export { type ChangeUserPassword } from './change-user-password.interface';
-export { type ChangeUserDisplayName } from './change-user-displayname.interface';
-export { type User } from './user-basic-information'
+} from "./restore-password.interface";
+export { type ChangeUserPassword } from "./change-user-password.interface";
+export { type ChangeUserDisplayName } from "./change-user-displayname.interface";
+export { type User } from "./user-basic-information";
