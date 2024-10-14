23872a774d6e270a34f4f5f98d507db646a33b0d
Diego Coscolla <dcoscolla@intermediait.com>
2024-10-14 11:31:51 -0300

    Fix tests

--- apps/extension/src/config/jest/setupTests.ts
+++ apps/extension/src/config/jest/setupTests.ts
@@ -3,3 +3,24 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+global.chrome = {
+  storage: {
+    local: {
+      get: jest.fn().mockImplementationOnce((_, callback) =>
+        callback({
+          accessToken: 'mockAccessToken',
+        }),
+      ),
+    },
+  },
+  runtime: {
+    connect: jest.fn().mockReturnValue({
+      postMessage: jest.fn(),
+      onMessage: {
+        addListener: jest.fn(),
+      },
+    }),
+  },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+} as any;
