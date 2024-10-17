c32cde91251280ce7ff09fcc43384db955d8efcd
Juan Ignacio Martel <jmartel@intermediait.com>
2024-10-17 14:02:42 -0300

    Fix error on test file

--- apps/extension/src/screens/__tests__/login.screen.test.tsx
+++ apps/extension/src/screens/__tests__/login.screen.test.tsx
@@ -61,11 +61,6 @@ global.chrome = {
       get: jest.fn().mockReturnValue('http://example.com'),
     },
   },
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-} as any;
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(global as any).browser = {
   runtime: {
     sendNativeMessage: jest.fn(),
   },
