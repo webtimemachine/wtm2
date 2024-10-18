949cfff828dfa95169a3284d99aaf49263432dea
Juan Ignacio Martel <jmartel@intermediait.com>
2024-10-18 12:45:59 -0300

    Upgrade version

--- apps/extension/manifest-firefox.ts
+++ apps/extension/manifest-firefox.ts
@@ -3,7 +3,7 @@ import { ManifestV3Export } from '@crxjs/vite-plugin';
 export const manifestFirefox: ManifestV3Export = {
   manifest_version: 3,
   name: 'WebTM',
-  version: '1.6.1',
+  version: '1.6.2',
   description:
     'WebTM is a cross-platform solution to integrate the navigation history between desktop and mobile web browsers.',
   browser_specific_settings: {
