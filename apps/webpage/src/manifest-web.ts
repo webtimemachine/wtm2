493924f2dd22e3f0253a9aa38e0c7ba503bfccd1
Juan Ignacio Martel <jmartel@intermediait.com>
2024-10-29 14:41:21 -0300

    Update code to 1.6.6

--- apps/webpage/src/manifest-web.ts
+++ apps/webpage/src/manifest-web.ts
@@ -1,6 +1,6 @@
 export const manifestWeb = {
   name: 'WebTM',
-  version: '1.6.5',
+  version: '1.6.6',
   description:
     'WebTM is a cross-platform solution to integrate the navigation history between desktop and mobile web browsers.',
 };
