2d7e6d37f9d248b21fd724d59c986adc9c266b08
Diego Coscolla <dcoscolla@intermediait.com>
2024-11-13 12:45:30 -0300

    Add Discord webhook integration for contact form submissions

--- apps/webpage/src/app/(landing)/contact-us/page.tsx
+++ apps/webpage/src/app/(landing)/contact-us/page.tsx
@@ -1,6 +1,15 @@
 import { Metadata } from 'next';
-import { ContactForm } from './contact-form';
-import { Heading, Stack } from '@chakra-ui/react';
+
+import {
+  Button,
+  FormControl,
+  Heading,
+  Input,
+  Select,
+  Stack,
+  Textarea,
+} from '@chakra-ui/react';
+import { sendMessageToDiscord } from './action';
 
 export const metadata: Metadata = {
   title: 'WebTM | Contact Us',
@@ -16,7 +25,59 @@ export default function ContactUsPage() {
           Contact Us
         </Heading>
         <Stack justifyContent={'center'}>
-          <ContactForm />
+          <form id='contact-form' action={sendMessageToDiscord}>
+            <FormControl isRequired>
+              <Stack
+                wrap={'wrap'}
+                justifyContent={'center'}
+                direction={'row'}
+                gap={6}
+              >
+                <Input
+                  type='text'
+                  borderColor={'gray.300'}
+                  id='name'
+                  placeholder='Your Name'
+                  name='name'
+                />
+                <Input
+                  type='email'
+                  borderColor={'gray.300'}
+                  id='email'
+                  placeholder='Your Email'
+                  name='email'
+                />
+                <Select
+                  borderColor={'gray.300'}
+                  id='query-type'
+                  name='query-type'
+                >
+                  <option value=''>-- Please select an option --</option>
+                  <option value='Technical Support'>Technical Support</option>
+                  <option value='Delete Account Request'>
+                    Delete Account Request
+                  </option>
+                  <option value='Other'>Other</option>
+                </Select>
+                <Input
+                  type='text'
+                  borderColor={'gray.300'}
+                  id='subject'
+                  placeholder='Subject'
+                  name='subject'
+                />
+                <Textarea
+                  placeholder='Leave a message here'
+                  borderColor={'gray.300'}
+                  id='message'
+                  name='message'
+                />
+                <Button colorScheme='blue' type='submit'>
+                  Send Message
+                </Button>
+              </Stack>
+            </FormControl>
+          </form>
         </Stack>
       </Stack>
     </Stack>
