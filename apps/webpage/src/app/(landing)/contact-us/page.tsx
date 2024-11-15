b6486610983e0cbea3111448a0a5ae0d3c9e5647
Diego Coscolla <dcoscolla@intermediait.com>
2024-11-15 11:53:37 -0300

    Add contact form component and refactor contact us page

--- apps/webpage/src/app/(landing)/contact-us/page.tsx
+++ apps/webpage/src/app/(landing)/contact-us/page.tsx
@@ -11,6 +11,8 @@ import {
 } from '@chakra-ui/react';
 import { sendMessageToDiscord } from './action';
 
+import { ContactForm } from './form';
+
 export const metadata: Metadata = {
   title: 'WebTM | Contact Us',
   description:
@@ -25,59 +27,7 @@ export default function ContactUsPage() {
           Contact Us
         </Heading>
         <Stack justifyContent={'center'}>
-          <form id='contact-form' action={sendMessageToDiscord}>
-            <FormControl isRequired>
-              <Stack
-                wrap={'wrap'}
-                justifyContent={'center'}
-                direction={'row'}
-                gap={6}
-              >
-                <Input
-                  type='text'
-                  borderColor={'gray.300'}
-                  id='name'
-                  placeholder='Your Name'
-                  name='name'
-                />
-                <Input
-                  type='email'
-                  borderColor={'gray.300'}
-                  id='email'
-                  placeholder='Your Email'
-                  name='email'
-                />
-                <Select
-                  borderColor={'gray.300'}
-                  id='query-type'
-                  name='query-type'
-                >
-                  <option value=''>-- Please select an option --</option>
-                  <option value='Technical Support'>Technical Support</option>
-                  <option value='Delete Account Request'>
-                    Delete Account Request
-                  </option>
-                  <option value='Other'>Other</option>
-                </Select>
-                <Input
-                  type='text'
-                  borderColor={'gray.300'}
-                  id='subject'
-                  placeholder='Subject'
-                  name='subject'
-                />
-                <Textarea
-                  placeholder='Leave a message here'
-                  borderColor={'gray.300'}
-                  id='message'
-                  name='message'
-                />
-                <Button colorScheme='blue' type='submit'>
-                  Send Message
-                </Button>
-              </Stack>
-            </FormControl>
-          </form>
+          <ContactForm action={sendMessageToDiscord} />
         </Stack>
       </Stack>
     </Stack>
