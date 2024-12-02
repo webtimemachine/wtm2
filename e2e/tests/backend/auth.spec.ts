6fcda9a847fc4da61a7fffad31748198ef0a1c8c
Diego Coscolla <dcoscolla@intermediait.com>
2024-12-02 14:17:22 -0300

    feat: add environment configuration for end-to-end testing and update test cases

--- e2e/tests/backend/auth.spec.ts
+++ e2e/tests/backend/auth.spec.ts
@@ -1,24 +1,27 @@
 import { test, expect } from '@playwright/test';
+import { envConfig } from '../../utils/envConfig';
 
-const BACKEND_BASE_URL = 'http://localhost:5001';
+console.log(envConfig);
 
 test.describe('Auth', () => {
-  test('Should register successfully', async ({ request }) => {
-    const response = await request.post(`${BACKEND_BASE_URL}/api/auth/signup`, {
-      data: {
-        email: 'test@test.com',
-        password: 'Test@1234',
-        displayname: 'Test User',
-      },
-    });
-
-    console.log(response);
+  test('Should login successfully', async ({ request }) => {
+    const response = await request.post(
+      `${envConfig.E2E_BACKEND_BASE_URL}/api/auth/login`,
+      {
+        data: {
+          deviceKey: 'test',
+          userAgent: 'test',
+          userAgentData: 'test',
+          email: envConfig.E2E_TEST_EMAIL,
+          password: envConfig.E2E_TEST_PASSWORD,
+        },
+      }
+    );
 
     expect(response.ok()).toBeTruthy();
 
     const body = await response.json();
-    expect(body).toHaveProperty('email');
-    expect(body).toHaveProperty('id');
-    expect(body).toHaveProperty('partialToken');
+    expect(body).toHaveProperty('accessToken');
+    expect(body).toHaveProperty('refreshToken');
   });
 });
--- e2e/tests/webpage/landing.spec.ts
+++ e2e/tests/webpage/landing.spec.ts
@@ -1,15 +1,14 @@
 import { test, expect } from '@playwright/test';
-
-const BASE_URL = 'http://localhost:3000';
+import { envConfig } from '../../utils/envConfig';
 
 test.describe('Landing page', () => {
   test('Home page loads successfully', async ({ page }) => {
-    await page.goto(`${BASE_URL}/`);
+    await page.goto(`${envConfig.E2E_WEBPAGE_BASE_URL}/`);
     await expect(page).toHaveTitle('WebTM | Home');
   });
 
   test('Landing page has a dashboard button', async ({ page }) => {
-    await page.goto(`${BASE_URL}/`);
+    await page.goto(`${envConfig.E2E_WEBPAGE_BASE_URL}/`);
     const dashboardButton = page.locator('text=Dashboard');
     await expect(dashboardButton).toBeVisible();
   });
@@ -17,10 +16,10 @@ test.describe('Landing page', () => {
   test('Clicking dashboard button without session navigates to login page', async ({
     page,
   }) => {
-    await page.goto(`${BASE_URL}/`);
+    await page.goto(`${envConfig.E2E_WEBPAGE_BASE_URL}/`);
     const dashboardButton = page.locator('text=Dashboard');
     await expect(dashboardButton).toBeVisible();
     await dashboardButton.click();
-    await expect(page).toHaveURL(`${BASE_URL}/login`);
+    await expect(page).toHaveURL(`${envConfig.E2E_WEBPAGE_BASE_URL}/login`);
   });
 });
