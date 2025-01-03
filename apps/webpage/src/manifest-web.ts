d192d81ee3435245556c0d6dd2d583df549cfcf7
Bautista Carpintero <bcarpintero@intermediait.com>
2025-01-03 11:31:47 -0300

    Updata code to 1.7.4

--- apps/webpage/src/manifest-web.ts
+++ apps/webpage/src/manifest-web.ts
@@ -1,6 +1,6 @@
 export const manifestWeb = {
   name: 'WebTM',
-  version: '1.7.3',
+  version: '1.7.4',
   description:
     'WebTM is a cross-platform solution to integrate the navigation history between desktop and mobile web browsers.',
 };
